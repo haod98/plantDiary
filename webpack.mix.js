b001ae509d41006818ee2b343f93ecc98092c72a
Hao <74541600+haod98@users.noreply.github.com>
2022-05-24 20:04:25 +0200

    chore: disable notifications after compiling

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,4 @@
-const mix = require('laravel-mix');
+const mix = require("laravel-mix");
 
 /*
  |--------------------------------------------------------------------------
@@ -11,15 +11,17 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js').vue()
-    .postCss('resources/css/app.css', 'public/css', [
-        require('postcss-import'),
-        require('tailwindcss'),
+mix.js("resources/js/app.js", "public/js")
+    .vue()
+    .postCss("resources/css/app.css", "public/css", [
+        require("postcss-import"),
+        require("tailwindcss"),
     ])
     .alias({
-        '@': 'resources/js',
+        "@": "resources/js",
     });
 
 if (mix.inProduction()) {
     mix.version();
 }
+mix.disableSuccessNotifications();
