56a679e19bd3be41807b372f0f53db7b6789081c
Hao <74541600+haod98@users.noreply.github.com>
2022-05-28 13:12:50 +0200

    feat: add simple form

---
+++ resources/js/Pages/Plants/PlantsForm.vue
@@ -0,0 +1,24 @@
+<script>
+import { Inertia } from "@inertiajs/inertia";
+import { reactive } from "vue";
+
+export default {
+    data() {
+        return {
+            firstName: "",
+        };
+    },
+    methods: {
+        submit() {
+            Inertia.post("/plants/store", this.firstName);
+        },
+    },
+};
+</script>
+<template>
+    <form @submit.prevent>
+        <label for="plantName">Plant Name:</label>
+        <input v-model="plantName" type="text" />
+        <button type="submit" @click="submit"></button>
+    </form>
+</template>
