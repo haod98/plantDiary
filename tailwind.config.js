41639e0c2073e46ca82e334aa202a76f7cf18861
Hao <74541600+haod98@users.noreply.github.com>
2022-05-28 11:44:31 +0200

    chore: add colors and fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,36 @@
-const defaultTheme = require('tailwindcss/defaultTheme');
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
     content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './vendor/laravel/jetstream/**/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
-        './resources/js/**/*.vue',
+        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+        "./vendor/laravel/jetstream/**/*.blade.php",
+        "./storage/framework/views/*.php",
+        "./resources/views/**/*.blade.php",
+        "./resources/js/**/*.vue",
     ],
 
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Nunito', ...defaultTheme.fontFamily.sans],
+                sans: [
+                    "expressway",
+                    "new-order",
+                    "Nunito",
+                    ...defaultTheme.fontFamily.sans,
+                ],
+            },
+            colors: {
+                plantDiary: {
+                    50: "#A6D6AA",
+                    100: "#7CA982",
+                    150: "#6A9D71",
+                },
             },
         },
     },
 
-    plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
+    plugins: [
+        require("@tailwindcss/forms"),
+        require("@tailwindcss/typography"),
+    ],
 };
