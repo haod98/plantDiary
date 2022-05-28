68b7e2cae4d12dd20f4b9ff50e61bcb717cd214a
Hao <74541600+haod98@users.noreply.github.com>
2022-05-28 15:58:02 +0200

    chore: update form style

--- resources/js/Pages/Plants/PlantsForm.vue
+++ resources/js/Pages/Plants/PlantsForm.vue
@@ -1,6 +1,6 @@
 <script>
 import { Inertia } from "@inertiajs/inertia";
-import { useForm } from "@inertiajs/inertia-vue3";
+import { useForm, Head } from "@inertiajs/inertia-vue3";
 import AppLayout from "../../Layouts/AppLayout.vue";
 
 export default {
@@ -14,45 +14,60 @@ export default {
         });
         return { form };
     },
-    components: { AppLayout },
+    components: { AppLayout, Head },
 };
 </script>
 <template>
     <AppLayout>
-        <form @submit.prevent="form.post(route('plants.store'))">
-            <label for="plantName">Plant Name:</label>
-            <input v-model="form.plantName" id="plantName" type="text" />
-            <label for="description">Plant Description:</label>
-            <textarea
-                name="description"
-                id="description"
-                cols="30"
-                rows="10"
-                v-model="form.description"
-            ></textarea>
-            <label for="days-to-water">Days to water:</label>
-            <input
-                v-model="form.daysToWater"
-                type="number"
-                id="days-to-water"
-            />
-            <label for="water-count">How much water:</label>
-            <select
-                v-model="form.waterCount"
-                name="water-count"
-                id="water-count"
-            >
-                <option value="0" selected>None</option>
-                <option value="1">Low</option>
-                <option value="2">Medium</option>
-                <option value="3">High</option>
-            </select>
-            <select v-model="form.sun" name="sun" id="sun">
-                <option value="0" selected>None</option>
-                <option value="1">Shadow</option>
-                <option value="2">Half-shadow</option>
-                <option value="3">Sun</option>
-            </select>
+        <Head title="New Plant" />
+        <form
+            @submit.prevent="form.post(route('plants.store'))"
+            class="flex flex-col gap-4 p-4"
+        >
+            <div class="flex flex-col">
+                <label for="plantName">Plant Name:</label>
+                <input v-model="form.plantName" id="plantName" type="text" />
+            </div>
+            <div class="flex flex-col">
+                <label for="description">Plant Description:</label>
+                <textarea
+                    name="description"
+                    id="description"
+                    cols="30"
+                    rows="10"
+                    v-model="form.description"
+                ></textarea>
+            </div>
+            <div class="flex flex-col">
+                <label for="days-to-water">Days to water:</label>
+                <input
+                    v-model="form.daysToWater"
+                    type="number"
+                    id="days-to-water"
+                />
+            </div>
+            <div class="flex flex-col">
+                <label for="water-count">How much water:</label>
+                <select
+                    v-model="form.waterCount"
+                    name="water-count"
+                    id="water-count"
+                >
+                    <option value="0" selected>None</option>
+                    <option value="1">Low</option>
+                    <option value="2">Medium</option>
+                    <option value="3">High</option>
+                </select>
+            </div>
+            <div class="flex flex-col">
+                <label for="sun">How much sun?</label>
+                <select v-model="form.sun" name="sun" id="sun">
+                    <option value="0" selected>None</option>
+                    <option value="1">Shadow</option>
+                    <option value="2">Half-shadow</option>
+                    <option value="3">Sun</option>
+                </select>
+            </div>
             <button type="submit">Submit</button>
         </form>
     </AppLayout>
