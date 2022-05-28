fb0e7dc509aa26718dda681cb123728754b17a44
Hao <74541600+haod98@users.noreply.github.com>
2022-05-28 12:51:19 +0200

    chore: add App

---
+++ resources/js/Pages/App.vue
@@ -0,0 +1,28 @@
+<script setup>
+import NavbarComponent from "./components/NavbarComponent.vue";
+import HeroSection from "./components/HeroSection.vue";
+import FeatureComponent from "./components/FeatureComponent.vue";
+import PlantCardComponent from "./components/PlantCardComponent.vue";
+import PrimaryButton from "./components/buttons/PrimaryButton.vue";
+import FooterComponent from "./components/FooterComponent.vue";
+import HomeView from "./HomeView.vue";
+import { defineComponent } from "vue";
+defineComponent({
+    components: {
+        NavbarComponent,
+    },
+});
+</script>
+
+<template>
+    <header class="mb-3 px-4 lg:px-10">
+        <NavbarComponent />
+    </header>
+
+    <main class="px-4 lg:px-10">
+        <HomeView />
+    </main>
+    <footer>
+        <FooterComponent />
+    </footer>
+</template>
