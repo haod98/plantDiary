dc6940c84a2c7f1c6e1f4e6fc2b1a863962ea8b2
Hao <74541600+haod98@users.noreply.github.com>
2022-05-30 19:30:46 +0200

    chore: change order of migrations

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -22,6 +22,8 @@ return new class extends Migration
             $table->rememberToken();
             $table->foreignId('current_team_id')->nullable();
             $table->string('profile_photo_path', 2048)->nullable();
+            $table->unsignedBigInteger('room_id')->nullable();
+            $table->foreign('room_id')->references('id')->on('rooms');
             $table->timestamps();
         });
     }
