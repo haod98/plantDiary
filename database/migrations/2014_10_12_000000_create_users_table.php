432095b69f3af205ac9f390c12f9e3d435849cd8
Hao <74541600+haod98@users.noreply.github.com>
2022-05-30 20:12:21 +0200

    fix: wrong foreign key

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -22,8 +22,6 @@ return new class extends Migration
             $table->rememberToken();
             $table->foreignId('current_team_id')->nullable();
             $table->string('profile_photo_path', 2048)->nullable();
-            $table->unsignedBigInteger('room_id')->nullable();
-            $table->foreign('room_id')->references('id')->on('rooms');
             $table->timestamps();
         });
     }
--- database/migrations/2014_10_12_3000000_create_rooms_table.php
+++ database/migrations/2014_10_12_3000000_create_rooms_table.php
@@ -16,6 +16,8 @@ return new class extends Migration
         Schema::create('rooms', function (Blueprint $table) {
             $table->id();
             $table->string("room");
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
             $table->timestamps();
         });
     }
