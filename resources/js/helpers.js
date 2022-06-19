b1b230652b1c2ccdb260ceb7d8cdf580c4675547
Hao <74541600+haod98@users.noreply.github.com>
2022-06-19 14:17:12 +0200

    refactor: add date formater in helpers

--- resources/js/helpers.js
+++ resources/js/helpers.js
@@ -1,3 +1,9 @@
+import { format } from "date-fns";
+
 export function loadImage(imageURI) {
     return `/plantImages?image=${encodeURIComponent(imageURI)}`;
+}
+
+export function formatDate(date, dateFormat = "dd.MM.yyyy") {
+    return format(new Date(date), dateFormat);
 }
