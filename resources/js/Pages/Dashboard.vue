5093b78acd29cf0dbfa6d493b5721edee4d1a945
Hao <74541600+haod98@users.noreply.github.com>
2022-06-21 18:19:02 +0200

    chore: remove unused content

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -32,13 +32,6 @@ export default {
                 {{ $page.props.flash.success }}
             </p>
         </div>
-        <div class="py-12">
-            <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
-                <div class="overflow-hidden bg-white shadow-xl sm:rounded-lg">
-                    <Welcome />
-                </div>
-            </div>
-        </div>
         <Plants :plants="plants" />
     </AppLayout>
 </template>
