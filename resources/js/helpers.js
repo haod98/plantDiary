4b2f63b033b7fc4b5f5b6f90e3d288e830e1e043
Hao <74541600+haod98@users.noreply.github.com>
2022-06-22 11:07:29 +0200

    refactor: moved remainingDays function to helper

--- resources/js/helpers.js
+++ resources/js/helpers.js
@@ -1,4 +1,4 @@
-import { format } from "date-fns";
+import { format, differenceInDays } from "date-fns";
 
 export function loadImage(imageURI) {
     return `/plantImages?image=${encodeURIComponent(imageURI)}`;
@@ -6,4 +6,9 @@ export function loadImage(imageURI) {
 
 export function formatDate(date, dateFormat = "dd.MM.yyyy") {
     return format(new Date(date), dateFormat);
+}
+
+export function calculateRemainingDays(nextTimeToWater) {
+    if (nextTimeToWater === null) return null;
+    return differenceInDays(new Date(nextTimeToWater), new Date()) + 1; // +1 so it also includes todays day.
 }
