f32d945974457a2a54f57f074b44dd6ae1a95663
Hao <74541600+haod98@users.noreply.github.com>
2022-06-23 12:41:24 +0200

    refactor: create homepage layout

---
+++ resources/js/Layouts/HomepageLayout.vue
@@ -0,0 +1,17 @@
+<script setup>
+import NavbarComponent from "../Pages/components/NavbarComponent.vue";
+import FooterComponent from "../Pages/components/FooterComponent.vue";
+</script>
+<template>
+    <header class="mb-3 px-4 lg:px-10">
+        <NavbarComponent />
+    </header>
+
+    <main class="px-4 lg:px-10">
+        <slot />
+    </main>
+
+    <footer>
+        <FooterComponent />
+    </footer>
+</template>
--- resources/js/Pages/App.vue
+++ resources/js/Pages/App.vue
@@ -1,26 +1,17 @@
 <script>
-import NavbarComponent from "./components/NavbarComponent.vue";
-import FooterComponent from "./components/FooterComponent.vue";
+import HomepageLayout from "../Layouts/HomepageLayout.vue";
 import HomeView from "./HomeView.vue";
 import { defineComponent } from "vue";
 export default defineComponent({
     components: {
-        NavbarComponent,
         HomeView,
-        FooterComponent,
+        HomepageLayout,
     },
 });
 </script>
 
 <template>
-    <header class="mb-3 px-4 lg:px-10">
-        <NavbarComponent />
-    </header>
-
-    <main class="px-4 lg:px-10">
+    <HomepageLayout>
         <HomeView />
-    </main>
-    <footer>
-        <FooterComponent />
-    </footer>
+    </HomepageLayout>
 </template>
