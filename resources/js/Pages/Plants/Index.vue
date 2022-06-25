64856a96160c4e5e5796d22015d11702519cfcab
Hao <74541600+haod98@users.noreply.github.com>
2022-06-25 18:38:10 +0200

    feat: create grid view for plants

--- resources/js/Pages/Plants/Index.vue
+++ resources/js/Pages/Plants/Index.vue
@@ -1,15 +1,19 @@
 <template>
-    <div v-for="plant in plants" :key="plant.id">
-        <PlantDashboard
-            :title="plant.name"
-            :description="plant.description"
-            :daysToWater="calculateRemainingDays(plant.next_water)"
-            :waterCount="plant.water_count"
-            :sun="plant.sun"
-            :plantImg="plant.plant_images"
-            :defaultDays="plant.days_to_water"
-            :plantId="plant.id"
-        />
+    <div
+        class="md:grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5"
+    >
+        <div v-for="plant in plants" :key="plant.id">
+            <PlantDashboard
+                :title="plant.name"
+                :description="plant.description"
+                :daysToWater="calculateRemainingDays(plant.next_water)"
+                :waterCount="plant.water_count"
+                :sun="plant.sun"
+                :plantImg="plant.plant_images"
+                :defaultDays="plant.days_to_water"
+                :plantId="plant.id"
+            />
+        </div>
     </div>
 </template>
 
