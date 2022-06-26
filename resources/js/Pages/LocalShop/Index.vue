22eef5df14d37e7b9d88332f07865a9c8f26529c
Hao <74541600+haod98@users.noreply.github.com>
2022-06-26 20:41:55 +0200

    fix: styling

--- resources/js/Pages/LocalShop/Index.vue
+++ resources/js/Pages/LocalShop/Index.vue
@@ -2,7 +2,9 @@
     <Head title="Local Shops" />
     <AppLayout>
         <template #header>
-            <h2>Local shops</h2>
+            <h2 class="text-xl font-semibold leading-tight text-gray-800">
+                Local shops
+            </h2>
         </template>
         <div class="my-5 mx-4">
             <JetLabel for="search">Search</JetLabel>
